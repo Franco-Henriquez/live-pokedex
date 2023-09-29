b3ca29aa7fca8dc54aab49dd3109c783f22c9334
Franco_H <francoh.jobs@gmail.com>
2023-09-29 00:53:48 -0400

    Initial commit

---
+++ src/main/java/com/example/livepokedex/LivePokedexApplication.java
@@ -0,0 +1,13 @@
+package com.example.livepokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivePokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivePokedexApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/livepokedex/models/Trainer.java
@@ -0,0 +1,10 @@
+package com.example.livepokedex.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="trainers")
+public class Trainer {
+	
+}
